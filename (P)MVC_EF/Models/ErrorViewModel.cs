ee6bd8893b0c54f7edd54154aeff9075b09a21ca
Zak <zak.henderson.47@gmail.com>
2023-11-16 12:11:05 -0600

    Add project files.

---
+++ (P)MVC_EF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _P_MVC_EF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
